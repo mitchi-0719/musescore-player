8097e263bad4eed1157d9590ea3987da8fdf471b
mitchi <pg.saku0719@gmail.com>
2026-05-19 23:21:49 +0900

    ビルドの設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    nodePolyfills({
+      exclude: ['vm'],
+      globals: {
+        Buffer: true,
+      },
+    }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('webmscore')) return 'webmscore'
+            if (id.includes('tone')) return 'audio-engine'
+            return 'vendor'
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
