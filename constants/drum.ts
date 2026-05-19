d70d59b702b5f1bdf23d6a32b4870dbc61fab146
mitchi <pg.saku0719@gmail.com>
2026-05-19 15:56:42 +0900

    refs #32 stash漏れとフォルダ名変更

---
+++ constants/drum.ts
@@ -0,0 +1,35 @@
+import * as Tone from 'tone'
+
+const DRUM_MAP: Record<number, string> = {
+  36: 'kick.wav', // バスドラム1
+  37: 'side-stick.wav', // Side Stick
+  38: 'snare.wav', // Acoustic Snare
+  41: 'tom-low-floor.wav', // Low Floor Tom
+  42: 'hihat-closed.wav', // Closed Hi-Hat
+  43: 'tom-high-floor.wav', // High Floor Tom
+  44: 'hihat-pedal.wav', // ペダル ハイハット
+  45: 'tom-low.wav', // Low Tom
+  46: 'hihat-open.wav', // Open Hi-Hat
+  47: 'tom-low-mid.wav', // ロー ミッド タム
+  48: 'tom-hi-mid.wav', // Hi-Mid Tom
+  49: 'crash1.wav', // Crash Cymbal 1
+  50: 'tom-high.wav', // High Tom
+  51: 'ride1.wav', // Ride Cymbal 1
+  52: 'china.wav', // China Cymbal
+  53: 'ride-bell.wav', // Ride Bell
+  54: 'tambourine.wav', // Tambourine
+  55: 'splash.wav', // Splash Cymbal
+  56: 'cowbell.wav', // Cowbell
+  57: 'crash2.wav', // Crash Cymbal 2
+  59: 'ride2.wav', // Ride Cymbal 2
+}
+
+export const initDrumSampler = () => {
+  return new Tone.Sampler({
+    urls: DRUM_MAP,
+    baseUrl: '/sounds/drums/',
+    onload: () => {
+      console.log('🥁 カスタムドラムキットのロード完了！')
+    },
+  }).toDestination()
+}
