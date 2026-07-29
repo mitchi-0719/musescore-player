e43afc1d5905268bb31b09e0324b22965dc4558c
mitchi <pg.saku0719@gmail.com>
2026-07-29 23:32:21 +0900

    feature #76 カーソルに追従するように

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,9 @@
 export const Header = () => {
   return (
-    <header className="sticky top-0 right-0 left-0 z-10 bg-gray-800 text-white">
+    <header
+      className="sticky top-0 right-0 left-0 z-10 bg-gray-800 text-white"
+      data-app-header
+    >
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
         <h1 className="text-xl font-bold">MuseScore Player</h1>
         <nav>
